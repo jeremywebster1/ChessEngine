95af3b8c1f3d22e35fab39300f1e57f725e3b128
jeremy <jerjon255@gmail.com>
2022-08-26 19:25:26 -0400

    now can play against it

--- js/main.js
+++ js/main.js
@@ -1,33 +1,31 @@
-var board = null;
-var game = new Chess();
+// NOTE: this example uses the chess.js library:
+// https://github.com/jhlywa/chess.js
 
-function makeRandomMove() {
-  // chess.js gives us all the possible moves in an array
-  // [ move1, move2, move3 ... ]
-  var possibleMoves = game.moves();
-  // filterForCaptures();
-  console.log(possibleMoves);
-  possibleMoves = filterForCaptures();
+var board = null
+var game = new Chess()
 
-  // exit if the game is over
-  if (game.game_over()) return;
+function onDragStart (source, piece, position, orientation) {
+  // do not pick up pieces if the game is over
+  if (game.game_over()) return false
 
-  // choses a random index in the list
-  var randomIdx = Math.floor(Math.random() * possibleMoves.length);
+  // only pick up pieces for White
+  if (piece.search(/^b/) !== -1) return false
+}
 
-  // updates javascript board state
-  game.move(possibleMoves[randomIdx]);
+function makeRandomMove () {
+  var possibleMoves = filterForCaptures();
 
-  // changes html board state
-  board.position(game.fen());
+  // game over
+  if (possibleMoves.length === 0) return
 
-  // call this function again in 5 secs
-  window.setTimeout(makeRandomMove, 500);
+  var randomIdx = Math.floor(Math.random() * possibleMoves.length)
+  game.move(possibleMoves[randomIdx])
+  board.position(game.fen())
 }
 
 function filterForCaptures() {
   var potentialMoves = game.moves().filter(move => move.includes("x") || move.includes("+"));
-
+ 
   if (potentialMoves.length == 0) {
     return game.moves();
   } else {
@@ -35,6 +33,33 @@ function filterForCaptures() {
   }
 }
 
-board = Chessboard("myBoard", "start");
 
-window.setTimeout(makeRandomMove, 500);
+function onDrop (source, target) {
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+  })
+
+  // illegal move
+  if (move === null) return 'snapback'
+
+  // make random legal move for black
+  window.setTimeout(makeRandomMove, 250)
+}
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+function onSnapEnd () {
+  board.position(game.fen())
+}
+
+var config = {
+  draggable: true,
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd
+}
+board = Chessboard('myBoard', config)
